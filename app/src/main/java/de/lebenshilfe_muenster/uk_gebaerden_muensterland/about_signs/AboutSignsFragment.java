b431d925416342b48df59ecb00077fcae34b3924
scaronthesky <scaronthesky@gmail.com>
2016-06-14 22:52:16 +0200

    #9 Adding WebView to AboutSignsFragment.

--- app/src/main/java/de/lebenshilfe_muenster/uk_gebaerden_muensterland/about_signs/AboutSignsFragment.java
+++ app/src/main/java/de/lebenshilfe_muenster/uk_gebaerden_muensterland/about_signs/AboutSignsFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import de.lebenshilfe_muenster.uk_gebaerden_muensterland.R;
 
@@ -27,12 +28,21 @@ import de.lebenshilfe_muenster.uk_gebaerden_muensterland.R;
  */
 public class AboutSignsFragment extends Fragment {
 
+    private WebView webView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.about_signs_fragment, container, false);
+        final View view = inflater.inflate(R.layout.about_signs_fragment, container, false);
+        this.webView = (WebView) view.findViewById(R.id.webView);
+        return view;
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        final String summary = "<html><body>You scored <b>192</b> points.</body></html>";
+        this.webView.loadData(summary, "text/html", null);
+    }
 }
 
