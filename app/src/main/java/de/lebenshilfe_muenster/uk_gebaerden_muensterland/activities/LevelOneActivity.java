887b468bb192ec8a8a42b74e4141846ea2797e2f
scaronthesky <scaronthesky@gmail.com>
2016-08-16 22:43:45 +0200

    #52 Adding copyright notices, minor change in about_signs.html

--- app/src/main/java/de/lebenshilfe_muenster/uk_gebaerden_muensterland/activities/LevelOneActivity.java
+++ app/src/main/java/de/lebenshilfe_muenster/uk_gebaerden_muensterland/activities/LevelOneActivity.java
@@ -14,7 +14,22 @@ import org.apache.commons.lang3.Validate;
 
 import de.lebenshilfe_muenster.uk_gebaerden_muensterland.R;
 import de.lebenshilfe_muenster.uk_gebaerden_muensterland.sign_browser.video.SignVideoFragment;
-
+/**
+ * Copyright (c) 2016 Matthias Tonhäuser
+ * <p/>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 public class LevelOneActivity extends AppCompatActivity {
 
     public static final String FRAGMENT_TO_SHOW = "fragment_to_show";
--- app/src/main/java/de/lebenshilfe_muenster/uk_gebaerden_muensterland/sign_browser/search/SignSearchActivity.java
+++ app/src/main/java/de/lebenshilfe_muenster/uk_gebaerden_muensterland/sign_browser/search/SignSearchActivity.java
@@ -28,7 +28,22 @@ import de.lebenshilfe_muenster.uk_gebaerden_muensterland.database.SignDAO;
 import de.lebenshilfe_muenster.uk_gebaerden_muensterland.sign_browser.SignBrowserAdapter;
 import de.lebenshilfe_muenster.uk_gebaerden_muensterland.sign_browser.search.video.SignSearchVideoActivity;
 import de.lebenshilfe_muenster.uk_gebaerden_muensterland.sign_browser.video.SignVideoFragment;
-
+/**
+ * Copyright (c) 2016 Matthias Tonhäuser
+ * <p/>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 public class SignSearchActivity extends AppCompatActivity implements SignBrowserAdapter.OnSignBrowserAdapterSignClickedListener {
 
     public static final String QUERY = "sign_browser_search_query";
